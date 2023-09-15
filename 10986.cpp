ad43eeadb1796b55812e15b92495947a7fdb19f2
Chanwoong1 <jcw7468@gmail.com>
2023-09-15 22:06:41 +0900

    Refactor: 코드 스타일 변경

--- 10986.cpp
+++ 10986.cpp
@@ -1,38 +1,35 @@
-#include <iostream>
-#include <cstring>
 #include <algorithm>
-#include <vector>
 #include <cmath>
+#include <cstring>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-#define fast                          \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(0), cout.tie(0)
+#define fast                        \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0), cout.tie(0)
 #define ll long long
 
 // 나머지 합
-int main()
-{
-    fast;
-    // arr : 누적합 계산, counter : 누적합의 나머지 갯수
-    ll N, M, arr[1000001], counter[1001], answer = 0;
-    memset(arr, 0, sizeof(arr));
-    memset(counter, 0, sizeof(counter));
-    // arr[0]은 초기값으로 먼저 계산
-    cin >> N >> M >> arr[0];
-    arr[0] %= M;
-    counter[arr[0]] += 1;
-    // 나머지 처리 하면서 누적합 계산
-    for (int i = 1; i < N; i++)
-    {
-        cin >> arr[i];
-        arr[i] = (arr[i] + arr[i - 1]) % M;
-        counter[arr[i]] += 1;
-    }
-    // counter 배열을 통해 nC2 조합 계산
-    for (int i = 0; i < M; i++)
-        answer += (counter[i] * (counter[i] - 1)) / 2;
-    // 마지막에 counter[0]을 더해 주는 것은 연속 부분 구간이 처음부터일 경우 추가
-    cout << answer + counter[0];
+int main() {
+  fast;
+  // arr : 누적합 계산, counter : 누적합의 나머지 갯수
+  ll N, M, arr[1000001], counter[1001], answer = 0;
+  memset(arr, 0, sizeof(arr));
+  memset(counter, 0, sizeof(counter));
+  // arr[0]은 초기값으로 먼저 계산
+  cin >> N >> M >> arr[0];
+  arr[0] %= M;
+  counter[arr[0]] += 1;
+  // 나머지 처리 하면서 누적합 계산
+  for (int i = 1; i < N; i++) {
+    cin >> arr[i];
+    arr[i] = (arr[i] + arr[i - 1]) % M;
+    counter[arr[i]] += 1;
+  }
+  // counter 배열을 통해 nC2 조합 계산
+  for (int i = 0; i < M; i++) answer += (counter[i] * (counter[i] - 1)) / 2;
+  // 마지막에 counter[0]을 더해 주는 것은 연속 부분 구간이 처음부터일 경우 추가
+  cout << answer + counter[0];
 }
--- 1253.cpp
+++ 1253.cpp
@@ -1,57 +1,49 @@
-#include <iostream>
-#include <cstring>
 #include <algorithm>
-#include <vector>
 #include <cmath>
+#include <cstring>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-#define fast                          \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(0), cout.tie(0)
+#define fast                        \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0), cout.tie(0)
 #define ll long long
 
 // 좋다
-int main()
-{
-    fast;
-    int N, _answer = 0;
-    ll tmp, A[2001];
-    memset(A, 0, sizeof(A));
-    cin >> N;
-    for (int i = 0; i < N; i++)
-        cin >> A[i];
-    if (N == 1)
-    {
-        cout << 0;
-        return 0;
-    }
-    sort(A, A + N);
-    for (int i = 0; i < N; i++)
-    {
-        int _start = 0 == i ? 1 : 0, _end = N - 1 == i ? N - 2 : N - 1;
-        while (_start < _end)
-        {
-            if (A[_start] + A[_end] >= A[i])
-            {
-                if (A[_start] + A[_end] == A[i])
-                {
-                    if (_start == i)
-                        _start++;
-                    else if (_end == i)
-                        _end--;
-                    else
-                    {
-                        _answer++;
-                        break;
-                    }
-                }
-                else
-                    _end--;
-            }
-            else
-                _start++;
+int main() {
+  fast;
+  int N, _answer = 0;
+  ll tmp, A[2001];
+  memset(A, 0, sizeof(A));
+  cin >> N;
+  for (int i = 0; i < N; i++) cin >> A[i];
+  // 예외처리
+  if (N == 1) {
+    cout << 0;
+    return 0;
+  }
+  // 정렬
+  sort(A, A + N);
+  for (int i = 0; i < N; i++) {
+    // 투 포인터 설정
+    int _start = 0 == i ? 1 : 0, _end = N - 1 == i ? N - 2 : N - 1;
+    while (_start < _end) {
+      if (A[_start] + A[_end] > A[i]) _end--;
+      // 같을 경우의 조건이 다양함
+      else if (A[_start] + A[_end] == A[i]) {
+        if (_start == i)
+          _start++;
+        else if (_end == i)
+          _end--;
+        else {
+          _answer++;
+          break;
         }
+      } else
+        _start++;
     }
-    cout << _answer;
+  }
+  cout << _answer;
 }
