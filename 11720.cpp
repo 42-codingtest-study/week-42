b7d512994f5c08b7e70e931ab68f810031a26fd6
Y2IN <dpdls0228@daum.net>
2023-09-17 23:43:51 +0900

    doit 1,2

--- 11720.cpp
+++ 11720.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-// 구간 합
-using namespace std;
 
+using namespace std;
+// n  길이를 알 수 있고 로직을 고려 -> O(N) 연산
+//  n 만큼 크기의 배열을 만들어 구현 가능
 int main()
 {
     int n, sum = 0;
     string num;
-    cin >> n;
+    cin >> n; // 숫자의 개수 N
     cin >> num;
     for (int i = 0; i < n; i++)
     {
